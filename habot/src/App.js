1c4c720b39ce857ecdd28c0cb0dba6cde1f2b1a9
mohammadarbaab <kazmiwrite@gmail.com>
2024-11-30 16:45:05 +0530

    add buttons

--- habot/src/App.js
+++ habot/src/App.js
@@ -5,6 +5,7 @@ import './input.css'
 import Dashboard from './Pages/Dashboard/Dashboard'
 import YtDashboard from './Pages/YtDashboard/YtDashboard'
 import BuyersPost from './Pages/BuyersPost/BuyersPost'
+import GetVefified from './Pages/GetVerified/GetVefified'
 
 
 function App() {
@@ -15,7 +16,8 @@ function App() {
     <Hero></Hero>
     <Dashboard></Dashboard> */}
     {/* <YtDashboard videoId={videoId}></YtDashboard> */}
-    <BuyersPost></BuyersPost>
+    {/* <BuyersPost></BuyersPost> */}
+    <GetVefified></GetVefified>
     </div>
   )
 }
---
+++ habot/src/Pages/GetVerified/GetVefified.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function GetVefified() {
+  return (
+    <div className="w-full h-[200px] flex flex-col md:flex-row justify-between items-center bg-[#E8FBFF] md:pl-[100px] md:pr-[140px] px-4 py-4">
+    <h1 className="text-[37px] font-[600] leading-[40px] text-center md:text-left">
+      Let Suppliers Find You
+    </h1>
+    <button className="w-[216px] h-[54px] rounded-[5px] bg-[#EB7150] md:mt-4 md:mt-0">
+      Get Verified
+    </button>
+  </div>
+  
+  
+  );
+}
+
+export default GetVefified;
