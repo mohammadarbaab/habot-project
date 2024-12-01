6b73acb68992113a7d98b25c710d6407b457d524
mohammadarbaab <kazmiwrite@gmail.com>
2024-12-01 22:13:32 +0530

    add responsive

--- habot/src/App.js
+++ habot/src/App.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import Navbar from './Pages/Navbar/Navbar'
-import Hero from './Pages/Hero/Hero'
-import './input.css'
-import Dashboard from './Pages/Dashboard/Dashboard'
-import YtDashboard from './Pages/YtDashboard/YtDashboard'
-import BuyersPost from './Pages/BuyersPost/BuyersPost'
-import GetVefified from './Pages/GetVerified/GetVefified'
-import Footer from './Pages/Footer/Footer'
-
+import React from 'react';
+import Navbar from './Pages/Navbar/Navbar';
+import Hero from './Pages/Hero/Hero';
+import './input.css';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import YtDashboard from './Pages/YtDashboard/YtDashboard';
+import BuyersPost from './Pages/BuyersPost/BuyersPost';
+import GetVefified from './Pages/GetVerified/GetVefified';
+import Footer from './Pages/Footer/Footer';
 
 function App() {
-  const videoId = "i8tgRHXx4oQ";
+  const videoId = 'i8tgRHXx4oQ';
   return (
     <div>
-    {/* <Navbar></Navbar>
-    <Hero></Hero>
-    <Dashboard></Dashboard> */}
-    {/* <YtDashboard videoId={videoId}></YtDashboard> */}
-    {/* <BuyersPost></BuyersPost> */}
-    {/* <GetVefified></GetVefified> */}
-    <Footer></Footer>
+      {/* <Navbar></Navbar>
+      <Hero></Hero>
+      <Dashboard></Dashboard> */}
+      <YtDashboard videoId={videoId}></YtDashboard>
+      {/* <BuyersPost></BuyersPost>
+      <GetVefified></GetVefified>
+      <Footer></Footer> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- habot/src/Pages/YtDashboard/YtDashboard.js
+++ habot/src/Pages/YtDashboard/YtDashboard.js
@@ -5,18 +5,13 @@ import check from '../Asessts/checked.png';
 function YtDashboard({ videoId }) {
   return (
     <>
-      <div className="w-[90%] h-auto md:h-[523px] mx-auto flex flex-col md:flex-row lg:mb-2 bg-[#072F57] p-4 md:p-10 md:gap-10 ">
-        <div className="w-full lg:ml-4 md:w-1/2 flex justify-center items-center">
+      <div className="lg:w-[90%] lg:h-[553px] bg-[#072F57] mx-auto flex flex-wrap justify-center lg:flex-row flex-col">
+        {/* Youtube section */}
+        <div className="flex w-auto lg:flex-col lg:w-[45%] md:justify-center flex-wrap lg:justify-center justify-center overflow-hidden sm:py-4 py-4">
           <iframe
             src={`https://www.youtube.com/embed/${videoId}`}
             alt=""
-            style={{
-              top: 0,
-              left: 0,
-              width: '550px',
-              height: '310px',
-              borderRadius: '5px',
-            }}
+            className="md:w-[590px] md:h-[310px] lg:w-full lg:h-[310px] flex flex-wrap sm:w-[70%] sm:h-[310px] w-[80%] h-[310px]"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
@@ -24,47 +19,50 @@ function YtDashboard({ videoId }) {
           ></iframe>
         </div>
 
-        <div className="w-full md:w-1/2 flex flex-col  lg:h-[310px] md:mt-16 xl:ml-[30px] ">
-          <div className="mb-4 flex flex-row md:gap-10 ">
-            <h1 className="text-[20px] text-center md:text-[26px] font-[700] leading-[32px] text-[#EB7150]">
-              Buyer
-              <div className="md:w-[100px] lg:w-[202px] mt-[10px] h-[0.5px] border-b-4 border-[#EB7150]"></div>
-            </h1>
-            <h1 className="text-[20px] text-center md:text-[26px] font-[700] leading-[32px] text-white">
-              Supplier
-              <div className="md:w-[100px] lg:w-[202px]  mt-[10px] h-[0.5px]"></div>
-            </h1>
-          </div>
-
-          <div className="xl:mt-4">
-          <div className="flex items-start mb-3 md:w-[80%] xl:w-full">
-            <img
-              src={check}
-              className="text-green-500 text-xl mr-2 mt-1.5 flex-shrink-0"
-            />
-            <div className="flex-1 text-white text-[18px] font-[500]">
-              Post your requirements.
+        <div className="flex lg:flex-col lg:mt-0 lg:ml-10 lg:w-[43%] w-full lg:justify-center flex-wrap">
+          <div className="flex w-full lg:w-[90%] h-auto lg:h-[310px] lg:flex-col flex-wrap lg:ml-2 lg:gap-3 gap-6 mb-4">
+            {/* top section */}
+            <div className="w-[70%] mx-auto lg:w-[100%] lg:h-[47px] h-auto flex-wrap lg:justify-between md:mt-2 flex md:justify-center mt-4 justify-between">
+              <h1 className="font-[700] text-[26px] leading-[32px] text-[#EB7150] lg:text-center lg:w-[50%] w-[30%]">
+                Buyer
+                <div className="lg:w-[100%] md:w-[35%] sm:w-[50%] w-[100%] mt-[10px] h-[0.5px] border-b-4 border-[#EB7150]"></div>
+              </h1>
+              <h1 className="font-[700] text-[26px] leading-[32px] text-[white] lg:w-[50%] lg:text-center">
+                Supplier
+                <div className=""></div>
+              </h1>
             </div>
-          </div>
-          <div className="flex items-start mb-3 w-full md:w-[90%] xl:w-full">
-            <img
-              src={check}
-              className="text-green-500 text-xl mr-2 mt-1.5 flex-shrink-0"
-            />
-            <div className="flex-1 text-white text-[18px] font-[500]">
-              Sit back for multiple suppliers to contact you.
+            {/* paragraph section */}
+            <div className="lg:w-[100%] h-auto flex lg:flex-col md:flex-col lg:mt-8 flex-col px-4">
+              <div className="flex items-start mb-3 md:w-[80%] xl:w-full">
+                <img
+                  src={check}
+                  className="text-green-500 text-xl mr-2 mt-1.5 flex-shrink-0"
+                />
+                <div className="flex-1 text-white text-[18px] font-[500]">
+                  Post your requirements.
+                </div>
+              </div>
+              <div className="flex items-start mb-3 w-full md:w-[100%] xl:w-full">
+                <img
+                  src={check}
+                  className="text-green-500 text-xl mr-2 mt-1.5 flex-shrink-0"
+                />
+                <div className="flex-1 text-white text-[18px] font-[500]">
+                  Sit back for multiple suppliers to contact you.
+                </div>
+              </div>
+              <div className="flex items-start mb-3 w-full">
+                <img
+                  src={check}
+                  className="text-green-500 text-xl mr-2 mt-1.5 flex-shrink-0"
+                />
+                <div className="flex-1 text-white text-[18px] font-[500]">
+                  Choose among the suppliers based on the{' '}
+                  <span className="inline-block">rating and reviews.</span>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="flex items-start mb-3 w-full">
-            <img
-              src={check}
-              className="text-green-500 text-xl mr-2 mt-1.5 flex-shrink-0"
-            />
-            <div className="flex-1 text-white text-[18px] font-[500]">
-              Choose among the suppliers based on the{' '}
-              <span className="block mt-2">rating and reviews.</span>
-            </div>
-          </div>
           </div>
         </div>
       </div>
